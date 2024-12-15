614f8048ba5817d7b39c360200b3260febde49cf
pavel mansilla goyas <pavel.mansilla@gmail.com>
2024-12-15 12:40:31 -0500

    vercel adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   devToolbar: {enabled: false},
-  output:'server'
+  output:'server',
+  adapter: vercel({}),
 });
